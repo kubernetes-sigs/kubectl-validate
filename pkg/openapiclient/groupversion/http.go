18ac375ab90d3513cf8937dcbe9dfd72159f6849
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-30 23:06:20 +0200

    refactor: introduce groupversion package
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/openapiclient/groupversion/http.go
@@ -0,0 +1,34 @@
+package groupversion
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"k8s.io/client-go/openapi"
+)
+
+type httpGroupVersion struct {
+	uri string
+}
+
+func (gv *httpGroupVersion) Schema(contentType string) ([]byte, error) {
+	//TODO: responses use and respect ETAG. use a disk cache
+	req, err := http.NewRequest("GET", gv.uri, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("Accept", contentType)
+	// Make HTTP request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return io.ReadAll(resp.Body)
+}
+
+func NewForHttp(uri string) openapi.GroupVersion {
+	return &httpGroupVersion{uri}
+}
---
+++ pkg/openapiclient/groupversion/openapi.go
@@ -0,0 +1,26 @@
+package groupversion
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/openapi"
+	"k8s.io/kube-openapi/pkg/spec3"
+)
+
+type OpenApiGroupVersion struct {
+	*spec3.OpenAPI
+}
+
+func (gv *OpenApiGroupVersion) Schema(contentType string) ([]byte, error) {
+	if strings.ToLower(contentType) != runtime.ContentTypeJSON {
+		return nil, fmt.Errorf("only application/json content type is supported")
+	}
+	return json.Marshal(gv.OpenAPI)
+}
+
+func NewForOpenAPI(spec *spec3.OpenAPI) openapi.GroupVersion {
+	return &OpenApiGroupVersion{spec}
+}
