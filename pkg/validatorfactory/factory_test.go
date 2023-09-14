711fd557e646781a7783ecc21c97f7b66623b526
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2023-09-13 17:44:05 -0700

    crd test case

--- pkg/validatorfactory/factory_test.go
+++ pkg/validatorfactory/factory_test.go
@@ -19,25 +19,73 @@ func TestValidatorFactory_TestPatcher(t *testing.T) {
 		file   string
 		client openapi.Client
 		want   *unstructured.Unstructured
-	}{{
-		name:   "configmap",
-		file:   "../../testcases/manifests/configmap.yaml",
-		client: openapiclient.NewHardcodedBuiltins("1.27"),
-		want: &unstructured.Unstructured{
-			Object: map[string]interface{}{
-				"apiVersion": "v1",
-				"kind":       "ConfigMap",
-				"metadata": map[string]interface{}{
-					"name":              "myapp",
-					"finalizers":        []interface{}{"finalizers.compute.linkedin.com"},
-					"creationTimestamp": nil,
-				},
-				"data": map[string]interface{}{
-					"key": "value",
+	}{
+		{
+			name:   "configmap",
+			file:   "../../testcases/manifests/configmap.yaml",
+			client: openapiclient.NewHardcodedBuiltins("1.27"),
+			want: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"apiVersion": "v1",
+					"kind":       "ConfigMap",
+					"metadata": map[string]interface{}{
+						"name":              "myapp",
+						"finalizers":        []interface{}{"finalizers.compute.linkedin.com"},
+						"creationTimestamp": nil,
+					},
+					"data": map[string]interface{}{
+						"key": "value",
+					},
 				},
 			},
 		},
-	},
+		{
+			name:   "cronjob_crd",
+			file:   "./testdata/cronjob.yaml",
+			client: openapiclient.NewLocalCRDFiles(nil, "./testdata/crds/"),
+			want: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"apiVersion": "batch.tutorial.kubebuilder.io/v1",
+					"kind":       "CronJob",
+					"metadata": map[string]interface{}{
+						"name": "cronjob-sample",
+						"labels": map[string]interface{}{
+							"app.kubernetes.io/name":       "cronjob",
+							"app.kubernetes.io/instance":   "cronjob-sample",
+							"app.kubernetes.io/part-of":    "project",
+							"app.kubernetes.io/managed-by": "kustomize",
+							"app.kubernetes.io/created-by": "project",
+						},
+						"creationTimestamp": nil,
+					},
+					"spec": map[string]interface{}{
+						"schedule":                "*/1 * * * *",
+						"startingDeadlineSeconds": int64(60),
+						"concurrencyPolicy":       "Allow",
+						"jobTemplate": map[string]interface{}{
+							"spec": map[string]interface{}{
+								"template": map[string]interface{}{
+									"spec": map[string]interface{}{
+										"restartPolicy": "OnFailure",
+										"containers": []interface{}{
+											map[string]interface{}{
+												"name":  "hello",
+												"image": "busybox",
+												"args": []interface{}{
+													"/bin/sh",
+													"-c",
+													"date; echo Hello from the Kubernetes cluster",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -61,6 +109,7 @@ func TestValidatorFactory_TestPatcher(t *testing.T) {
 			assert.NoError(t, err)
 			res := validator.SchemaValidator().Validate(untyped.Object)
 			assert.Empty(t, res.Errors)
+			assert.Equal(t, tt.want, &untyped)
 		})
 	}
 }
